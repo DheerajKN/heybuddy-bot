54458739740c2001da88957a7914ad31438ede2b
KOLLAPUDI NAGENDRA DHEERAJ <500037782@DDN.UPES.AC.IN>
2018-01-20 22:41:21 +0530

    Successful Integeration of Weather and Climate Functionalities

---
+++ README.md
@@ -0,0 +1,15 @@
+# heybuddy-bot
+
+## Helper bot that provides quick info using chat on Slack Application
+
+**Developed for Slack** *Written in JS* `for Users`
+
+```bash
+ weather in <CityName>
+ convert 20BTC to inr
+ 10usd to inr
+```
+## Don't worry even if you had typos in them or used related words. heybuddy bot can identify their near meaning and give responses.
+
+Planning to add more Capabilities it can be easily enhanced and modified by including API's
+and integrating in the system for better processing.
---
+++ index.js
@@ -0,0 +1,167 @@
+var Botkit = require('botkit')
+var http= require('http')
+var request=require('request')
+var cheerio=require('cheerio')
+
+var slackToken=process.env.SLACK_TOKEN
+
+ var controller = Botkit.slackbot({
+   debug: false
+ })
+
+ controller.spawn({
+   token: slackToken
+
+ }).startRTM(function(err,bot,payload) {
+   if (err) {
+    throw new Error('Could not connect to Slack')
+  }
+console.log('Connected to Slack')
+})
+
+controller.hears(['hello','Hi','What\'s up??'], 'direct_message,direct_mention,mention', function(bot,message) {
+  bot.reply(message,'Hello there!! I\'m the Buddy bot who can help you in Weather and Currency Updates')
+  bot.reply(message,'Strucked Up!! Message me Help to know more about functionalities')
+});
+
+controller.hears(['[H|h]elp','What can you do??'], 'direct_message,direct_mention,mention', function(bot,message) {
+  bot.reply(message,'I can help you in Weather and Currency Updates by typing weather in <city> to know Weather Updates '+
+  'and convert val <sourceCurrency> to <destinationCurrency> for currency updates')
+});
+
+controller.hears(['weather in (.*)', 'weather of (.*)','(.*)\'s weather','climate in (.*)','climate of (.*)','(.*)\'s climate'], 'direct_message,direct_mention,mention', function(bot,message) {
+    var city = message.match[1]
+    console.log("city: "+city)
+    if(undefined === city || '' === city || null === city)
+    {
+        bot.reply(message.text,"Didn't you forgot the city name? I am really sorry, currently I can't guess your city.")
+    }
+    else{
+        var options = {
+            protocol : 'http:',
+            host : 'api.openweathermap.org',
+            path : '/data/2.5/weather?q='+city+'&appid=7bf212dface8e17114014161ddce3318',
+            port : 80,
+            method : 'GET'
+          }
+
+        var request = http.request(options, function(response){
+            var body = ""
+            response.on('data', function(data) {
+                body += data
+                weather = JSON.parse(body)
+                var Temp= Math.round(weather.main.temp - 273.15) + ' °C'
+                console.log("weather :" + weather.weather[0].main)
+                msg="The Temp is "+ Temp + "and In " + city
+                var reaction = ""
+                switch(weather.weather[0].main)
+                {
+                        case "Clear":
+                                bot.reply(message,msg+" it's Clear Sky")
+                                reaction = "mostly_sunny"
+                                bot.reply(message,":"+reaction+":")
+                                bot.reply(message,"Its good idea to wear sunglasses before going out")
+                                break
+                        case "Clouds":
+                        case "Cloud":
+                                bot.reply(message,msg+" it's Raining")
+                                reaction = "cloud"
+                                bot.reply(message,":"+reaction+":")
+                                break
+                        case "Smoke":
+                                bot.reply(message,msg+" it's Foggy")
+                                reaction = "smoking"
+                                bot.reply(message,":"+reaction+":")
+                                bot.reply("Wear Proper Face Mask before leaving home")
+                                break
+                        case "Rain":
+                                bot.reply(message,msg+" It's Raining")
+                                reaction = "rain_cloud"
+                                bot.reply(message,":"+reaction+":")
+                                bot.reply(message,"Please carry umbrella if you are in " + city)
+                                break
+                        case "Thunderstorm":
+                                bot.reply(message,msg+" It's gonna be Thunderous Rain")
+                                reaction = "thunder_cloud_and_rain"
+                                bot.reply(message,":"+reaction+":")
+                                bot.reply(message,"Please don't go out if you are in " + city)
+                                break
+                        case "Drizzle":
+                              bot.reply(message,msg+" It's Drizzling")
+                              reaction = "partly_sunny_rain"
+                              bot.reply(message,":"+reaction+":")
+                              bot.reply(message,"Carry Umbrella it should be fine in " + city)
+                              break
+                        case "Haze":
+                              bot.reply(message,"The Temp is "+ Temp)
+                              reaction = "barely_sunny"
+                              bot.reply(message,":"+reaction+":")
+                              bot.reply(message,"There would be partly sunny in " + city)
+                              break
+                }
+                bot.api.reactions.add({
+                    timestamp: message.ts,
+                    channel: message.channel,
+                    name: reaction,
+                }, function(err, res) {
+                    if (err) {
+                        bot.botkit.log('Failed to add emoji reaction :(', err)
+                    }
+                })
+            })
+            response.on('end', function() {
+              /*res.send(JSON.parse(body)) */
+            })
+          })
+          request.on('error', function(e) {
+            console.log('Problem with request: ' + e.message)
+            bot.reply(message, "sorry, couldn't find weather info for city " + city)
+          })
+          request.end()
+  }
+});
+
+controller.hears(['convert (.*) (.*) to (.*)','(.*) (.*) to (.*)', 'How much is (.*) (.*) in (.*)','How much is (.*) (.*) to (.*)'], 'direct_message,direct_mention,mention', function(bot,message) {
+
+    var val = message.match[1]
+    var sourceval = message.match[2]
+    var destval = message.match[3]
+
+    if(undefined === sourceval || '' === sourceval || null === sourceval)
+    {
+        bot.reply(message.text,"Source Index not added")
+    }
+    else if(undefined === destval || '' === destval || null === destval)
+    {
+        bot.reply(message.text,"Destination Index not added")
+    }
+    else{
+      if(undefined === val || '' === val || null === val)
+      {
+          bot.reply(message.text,"Conversion value not added so considering as 1")
+          val=1    }
+        var url="https://exchangerate.guru/"+sourceval.toLowerCase()+"/"+destval.toLowerCase()+"/"+val+"/"
+
+        request(url,function(err,resp,body){
+          var $=cheerio.load(body)
+          var valx=$('.form-control').get(1)
+          var $valx=$(valx).attr('value')
+          var currentTime = new Date()
+          var date = new Date()
+        currentDate = date.getDate() /// hears improve
+        month = date.getMonth() + 1
+        year = date.getFullYear()
+
+hour = date.getHours()
+min  = date.getMinutes()
+sec  = date.getSeconds()
+
+var week = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+bot.reply(message,"As of "+week[date.getDay()] +": "+ currentDate + "/" + month + "/" + year+" and "+ hour + ":" + min + ":" + sec)
+if($valx >=5000){
+bot.reply(message,val+" "+sourceval.toUpperCase()+" = "+$valx+" :money_mouth_face: "+destval.toUpperCase())
+}else if(val>5500){
+bot.reply(message,":moneybag:"+ val+" "+sourceval.toUpperCase()+" = "+$valx+" :money_with_wings:"+" "+destval.toUpperCase())}
+else{bot.reply(message,val+" "+sourceval.toUpperCase()+" = "+$valx +" "+destval.toUpperCase())}
+}) }
+});
