35115f30ff19e9fde6c685b39a33a48b7e2ce6f7
KOLLAPUDI NAGENDRA DHEERAJ <500037782@DDN.UPES.AC.IN>
2018-01-21 14:59:56 +0530

    Added Integeration for Time, Day, Date and UTC Related Requests

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ var Botkit = require('botkit')
 var http= require('http')
 var request=require('request')
 var cheerio=require('cheerio')
-var express= require('express')
-var path=require('path')
 
 var slackToken=process.env.SLACK_TOKEN
 
@@ -27,8 +25,10 @@ controller.hears(['hello','Hi','What\'s up??'], 'direct_message,direct_mention,m
 });
 
 controller.hears(['[H|h]elp','What can you do??'], 'direct_message,direct_mention,mention', function(bot,message) {
-  bot.reply(message,'I can help you in Weather and Currency Updates by typing weather in <city> to know Weather Updates '+
-  'and convert val <sourceCurrency> to <destinationCurrency> for currency updates')
+   bot.reply(message,'I can help you in Weather and Currency Updates')
+   bot.reply(message,'by typing weather in <city> to know Weather Updates ')
+   bot.reply(message,'convert val <sourceCurrency> to <destinationCurrency> for currency updates')
+   bot.reply(message,'Time in <city> to get their present Day, Date, Time and UTC information')
 });
 
 controller.hears(['weather in (.*)', 'weather of (.*)','(.*)\'s weather','climate in (.*)','climate of (.*)','(.*)\'s climate'], 'direct_message,direct_mention,mention', function(bot,message) {
@@ -36,13 +36,13 @@ controller.hears(['weather in (.*)', 'weather of (.*)','(.*)\'s weather','climat
     console.log("city: "+city)
     if(undefined === city || '' === city || null === city)
     {
-        bot.reply(message.text,"Didn't you forgot the city name? I am really sorry, currently I can't guess your city.")
+        bot.reply(message,"Didn't you forgot the city name? I am really sorry, currently I can't guess your city.")
     }
     else{
         var options = {
             protocol : 'http:',
             host : 'api.openweathermap.org',
-            path : '/data/2.5/weather?q='+city+'&appid=7bf212dface8e17114014161ddce3318',
+            path : '/data/2.5/weather?q='+city.replace(" ","+")+'&appid=7bf212dface8e17114014161ddce3318',
             port : 80,
             method : 'GET'
           }
@@ -52,7 +52,7 @@ controller.hears(['weather in (.*)', 'weather of (.*)','(.*)\'s weather','climat
             response.on('data', function(data) {
                 body += data
                 weather = JSON.parse(body)
-                var Temp= Math.round(weather.main.temp - 273.15) + ' °C'
+                var Temp= Math.round(weather.main.temp - 273.15) + '°C '
                 console.log("weather :" + weather.weather[0].main)
                 msg="The Temp is "+ Temp + "and In " + city
                 var reaction = ""
@@ -60,44 +60,37 @@ controller.hears(['weather in (.*)', 'weather of (.*)','(.*)\'s weather','climat
                 {
                         case "Clear":
                                 bot.reply(message,msg+" it's Clear Sky")
-                                reaction = "mostly_sunny"
-                                bot.reply(message,":"+reaction+":")
+                                bot.reply(message,":mostly_sunny:")
                                 bot.reply(message,"Its good idea to wear sunglasses before going out")
                                 break
                         case "Clouds":
                         case "Cloud":
                                 bot.reply(message,msg+" it's Raining")
-                                reaction = "cloud"
-                                bot.reply(message,":"+reaction+":")
+                                bot.reply(message,":cloud:")
                                 break
                         case "Smoke":
                                 bot.reply(message,msg+" it's Foggy")
-                                reaction = "smoking"
-                                bot.reply(message,":"+reaction+":")
+                                bot.reply(message,":smoking:")
                                 bot.reply("Wear Proper Face Mask before leaving home")
                                 break
                         case "Rain":
                                 bot.reply(message,msg+" It's Raining")
-                                reaction = "rain_cloud"
-                                bot.reply(message,":"+reaction+":")
+                                bot.reply(message,":rain_cloud:")
                                 bot.reply(message,"Please carry umbrella if you are in " + city)
                                 break
                         case "Thunderstorm":
                                 bot.reply(message,msg+" It's gonna be Thunderous Rain")
-                                reaction = "thunder_cloud_and_rain"
-                                bot.reply(message,":"+reaction+":")
+                                bot.reply(message,":thunder_cloud_and_rain:")
                                 bot.reply(message,"Please don't go out if you are in " + city)
                                 break
                         case "Drizzle":
                               bot.reply(message,msg+" It's Drizzling")
-                              reaction = "partly_sunny_rain"
-                              bot.reply(message,":"+reaction+":")
+                              bot.reply(message,":partly_sunny_rain:")
                               bot.reply(message,"Carry Umbrella it should be fine in " + city)
                               break
                         case "Haze":
                               bot.reply(message,"The Temp is "+ Temp)
-                              reaction = "barely_sunny"
-                              bot.reply(message,":"+reaction+":")
+                              bot.reply(message,":barely_sunny:")
                               bot.reply(message,"There would be partly sunny in " + city)
                               break
                 }
@@ -131,34 +124,33 @@ controller.hears(['convert (.*) (.*) to (.*)','(.*) (.*) to (.*)', 'How much is 
 
     if(undefined === sourceval || '' === sourceval || null === sourceval)
     {
-        bot.reply(message.text,"Source Index not added please type again!!")
+        bot.reply(message,"Source Index not added please type again!!")
     }
     else if(undefined === destval || '' === destval || null === destval)
     {
-        bot.reply(message.text,"Destination Index not added please type again!!")
+        bot.reply(message,"Destination Index not added please type again!!")
     }
     else{
       if(undefined === val || '' === val || null === val)
       {
-          bot.reply(message.text,"Conversion value not added so considering as 1")
+          bot.reply(message,"Conversion value not added so considering as 1")
           val=1    }
 
         request("https://exchangerate.guru/"+sourceval.toLowerCase()+"/"+destval.toLowerCase()+"/"+val+"/",function(err,resp,body){
           var $=cheerio.load(body)
           var valx=$('.form-control').get(1)
           var $valx=$(valx).attr('value')
-          var currentTime = new Date()
+
           var date = new Date()
-        currentDate = date.getDate() /// hears improve
-        month = date.getMonth() + 1
-        year = date.getFullYear()
 
-hour = date.getHours()
-min  = date.getMinutes()
-sec  = date.getSeconds()
-
+          sec  = date.getSeconds().toString()
+if(parseInt(date.getHours())>11){
+  sec=sec.concat(" PM")
+}else{
+  sec=sec.concat(" AM")
+}
 var week = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
-bot.reply(message,"As of "+week[date.getDay()] +": "+ currentDate + "/" + month + "/" + year+" and "+ hour + ":" + min + ":" + sec)
+bot.reply(message,"As of "+week[date.getDay()] +": "+ date.getDate() + "/" + date.getMonth() + 1 + "/" + date.getFullYear() +" and "+ date.getHours() + ":" + date.getMinutes() + ":" + sec)
 if($valx >=5000){
 bot.reply(message,val+" "+sourceval.toUpperCase()+" = "+$valx+" :money_mouth_face: "+destval.toUpperCase())
 }else if(val>5500){
@@ -167,19 +159,33 @@ else{bot.reply(message,val+" "+sourceval.toUpperCase()+" = "+$valx +" "+destval.
 }) }
 });
 
-controller.hears('time in (.*)', 'direct_message,direct_mention,mention', function(bot,message) {
+controller.hears(['time in (.*)','day in (.*)','date in (.*)','utc of (.*)'], 'direct_message,direct_mention,mention', function(bot,message) {
   var timeloc = message.match[1]
 
-  if(undefined === timeloc || '' === timeloc || null === timeloc)
+  if(timeloc == undefined || timeloc == '' || timeloc == null)
   {
       bot.reply(message.text,"Location not added please type again!!")
   }
 
-      request("https://www.timeanddate.com/worldclock/?query="+timeloc.toLowerCase(),function(err,resp,body){
-        var $=cheerio.load(body)
-        var timeval=$('td#p0.rbi')
-        var time =timeval.text()
-console.log(time)
-        bot.reply(message,"Day and Time in "+timeloc+" is: "+time)
-      })
+  request("https://time.is/"+timeloc.replace(" ","_"),function(err,resp,body){
+    var $=cheerio.load(body)
+
+    var timev=$('div#twd').text()
+    var secv=$('span#leaps.leap0').text()
+    if(parseInt(timev.substring(0,timev.indexOf(':')))>11){
+      secv=secv.concat(" PM")
+    }else{
+      secv=secv.concat(" AM")
+    }
+
+    var dayv=$('div#dd.w90.tr').text()
+
+    var utcv=$('.keypoints ul li').get(0)
+    var utcval=$(utcv).text()
+
+bot.reply(message,"Time in "+timeloc+" is "+timev.concat(secv))
+bot.reply(message,"Day, Date is "+dayv)
+bot.reply(message,"They follow: "+utcval)
+
+  })
 });
